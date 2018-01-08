e9097a4bb9d7bad05d903687e7971c9618c5ab57
Robin Kamboj <dev.robinkamboj@gmail.com>
2018-01-09 01:43:23 +0530

    send button and text field with icon theme

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +28,38 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: new AppBar(
           title: new Text("Friendlychat")
       ),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                    hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
